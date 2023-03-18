22876361cde0162e930c73aa8259169225a760a9
Shubham Tiwari <st7112001@gmail.com>
2023-03-18 22:55:27 +0530

    Firebase Integration

--- app/src/main/java/com/example/talkpractice/LoginActivity.java
+++ app/src/main/java/com/example/talkpractice/LoginActivity.java
@@ -6,12 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+
 public class LoginActivity extends AppCompatActivity {
+
+    GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
 
         findViewById(R.id.loginBtn).setOnClickListener(new View.OnClickListener() {
             @Override
